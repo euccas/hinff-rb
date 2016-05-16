0e075ce4aa2b1dea9ad19e0e1849331b4f51aa7f
euccas <euccas.chen@gmail.com>
2016-05-16 16:14:33 -0700

    update inflib

--- lib/inflib.rb
+++ lib/inflib.rb
@@ -10,11 +10,12 @@ require 'fileutils'
 require 'open3'
 require 'yaml'
 require 'net/smtp'
-if RUBY_PLATFORM.match(/linux/)
+require 'pg'
+
+if /linux/ =~ RUBY_PLATFORM
   require 'pty'
   require 'expect'
 end
-require 'pg'
 
 module Inflib
   # Your code goes here...
